2217617efdd36fa75f3bfb1dd9db25c2c0ebea78
KanakPendse14 <127370285+KanakPendse14@users.noreply.github.com>
2024-03-31 12:20:30 +0530

    second model integrating

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import numpy as np
+from sklearn.base import ClassifierMixin
 from model import load_model
 from backend.preprocess import preprocess_image
 
@@ -10,7 +11,29 @@ model = load_model()
 
 @app.route('/')
 def index():
-    return render_template('login.html')
+    return render_template('home.html')
+
+# Route to render the diabetes prediction page
+@app.route('/diabetes_predict', methods=['GET'])
+def diabetes_predict():
+    # Extract input data from the form
+    data = request.form
+    features = [data['pregnancies'], data['glucose'], data['blood_pressure'],
+                data['skin_thickness'], data['insulin'], data['bmi'],
+                data['diabetes_pedigree_function'], data['age']]
+    
+    # Reshape the features array for prediction
+    input_data = np.array(features).reshape(1, -1)
+
+    # Make predictions using the diabetes model
+    # Here, you need to use your trained diabetes prediction model
+    # Replace 'prediction' with actual predictions from your model
+    prediction = Classifier  # Replace this with your model prediction code
+
+    # Return the prediction result
+    return render_template('diabetes_prediction_result.html', prediction=prediction)
+    # return render_template('diabetespredictpage.html')
+
 
 @app.route('/upload/image', methods=['GET', 'POST'])
 def upload_image():     
@@ -47,6 +70,10 @@ def upload_image():
     
     # Render the upload page for GET requests
     return render_template('upload.html')
+
+@app.route('/home/page')
+def home_page():
+    return render_template('home.html')
 
 if __name__ == '__main__':
     app.run(debug=True, port=80)
